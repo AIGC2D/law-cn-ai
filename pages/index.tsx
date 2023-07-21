5579957a3b39a338db77d84be88dc9fa891dc901
Alone88 <anhaoyl@qq.com>
2023-07-21 08:18:40 +0000

    support aigc2d.com

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           <ul className="grid grid-cols-2 md:grid-cols-4 gap-3 mt-4 max-w-2xl mx-auto text-xs">
             <li>
               <Link
-                href="https://magickpen.com/?ref=lawcnai"
+                href="https://www.aigc2d.com/logo/logo-bg-512.png"
                 className=" 
               dark:bg-white/30
               dark:text-slate-900
@@ -55,9 +55,9 @@ export default function Home() {
                   width={100}
                   height="20"
                   className="w-full mb-1 h-5 object-contain"
-                  alt="MagickPen - 智能写作助手"
+                  alt="AIGC2D"
                 />
-                智能写作助手
+                AIGC2D
               </Link>
             </li>
             <li>
@@ -153,7 +153,7 @@ export default function Home() {
           <div className="flex items-center justify-center space-x-6 mt-4 md:m-0">
             <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
               <Link
-                href="https://github.com/lvwzhen/law-cn-ai"
+                href="https://github.com/aigc2d/law-cn-ai"
                 className="flex items-center justify-center"
               >
                 <Image src={'/github.svg'} width="24" height="24" alt="Github logo" />
@@ -161,38 +161,14 @@ export default function Home() {
             </div>
             <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
               <Link
-                href="https://discord.gg/zYHbmcRazd"
+                href="https://www.aigc2d.com"
                 className="flex items-center justify-center"
               >
-                <Image src={'/discord.svg'} width="24" height="24" alt="Discord logo" />
-              </Link>
-            </div>
-            <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-              <Link href="https://twitter.com/lvwzhen" className="flex items-center justify-center">
-                <Image src={'/twitter.svg'} width="24" height="24" alt="Twitter logo" />
+                <Image src={'https://www.aigc2d.com/logo/logo-bg-512.png'} width="24" height="24" alt="AIGC2D" />
               </Link>
             </div>
           </div>
         </div>
-
-        <a
-          href="https://discord.gg/zYHbmcRazd"
-          target="_blank"
-          className="fixed right-4 bottom-4 flex h-12 w-12 items-center justify-center rounded-full bg-black text-white hover:bg-gray-600"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10 3c-4.31 0-8 3.033-8 7 0 2.024.978 3.825 2.499 5.085a3.478 3.478 0 01-.522 1.756.75.75 0 00.584 1.143 5.976 5.976 0 003.936-1.108c.487.082.99.124 1.503.124 4.31 0 8-3.033 8-7s-3.69-7-8-7zm0 8a1 1 0 100-2 1 1 0 000 2zm-2-1a1 1 0 11-2 0 1 1 0 012 0zm5 1a1 1 0 100-2 1 1 0 000 2z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </a>
       </main>
     </>
   )
